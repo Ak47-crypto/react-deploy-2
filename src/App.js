0e8deeb5265059d5e6ad8dc72f4c4061cf37c9a9
Ak47-crypto <ak6550352@gmail.com>
2021-07-16 19:47:23 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,67 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    <header>
+    <h1>My first React Site</h1>
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Contact us</a>
+        <a href="#">Policy</a>
+    </nav>
+    </header>
+    {/*<img src="img.jpg" alt="Web design image"></img>*/}
+    <hr></hr>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <section>
+      <article>
+        <h2>
+          Front End Web dev
+        </h2>
+      <p className="p1">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab est earum
+          nulla. Quia temporibus quae at, ea recusandae enim? Quod illo corrupti
+          animi cum fugiat nostrum cupiditate repellat repellendus quas eius
+          inventore iusto, facere totam fugit dolorum provident tempora
+          consequatur quam veritatis mollitia omnis. Delectus quae aliquid
+          tempora, vero ratione placeat veniam nulla corrupti enim voluptatibus
+          voluptas inventore fugiat architecto doloribus necessitatibus odio
+          minima totam aperiam quod! Porro eum ratione voluptatem praesentium,
+          delectus ad. Maxime, unde aperiam. Iusto nam pariatur nulla ut, ullam,
+          harum in facilis id quae soluta voluptatum nemo unde cum similique
+          fuga, ipsa quis sequi. Cum, tempore.
+      </p>
+      </article>
+      <article>
+        <h2>
+          Back End Web Dev
+        </h2>
+      <p className="p2">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum
+          porro exercitationem quidem aspernatur deleniti tempore molestiae
+          ipsum soluta ullam? Adipisci atque officiis necessitatibus, rem quod
+          ea nemo dicta. Vel explicabo consectetur vero atque dolore eum
+          laudantium quos possimus tempore excepturi. Facilis quasi quaerat sint
+          totam obcaecati officia repellat nemo similique! Incidunt, itaque
+          dolores. Corrupti, deleniti ratione, dolor eaque aspernatur odit, sunt
+          error repellat voluptate nesciunt dicta non molestias recusandae
+          velit. Aperiam sapiente, odit dolorem mollitia sunt suscipit
+          distinctio quam ducimus neque vitae nesciunt cupiditate quasi,
+          reprehenderit repellendus, sit ex? Ex saepe, soluta amet alias est
+          maiores nisi ab a corrupti?
+      </p>
+      </article>
+      </section>
+
+    
+      <footer>
+        <p>Ankur AllRightReserved © 2021 - 2021</p>
+      </footer>
+  </div>
   );
 }
 
